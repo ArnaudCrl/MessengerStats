b912e7ca50e1dbdb3e965d39ea88bc5af85b51b2
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2018-10-23 21:56:23 +0200

    displays author, date of publication and message

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def write_to_csv(message):
+def write_to_csv(html_input):
     #csvfile = csv.writer(open("chatdata.csv", "w"))
     #csvfile.writerow(["date"])
-    file = open(message, encoding="utf8")
+    file = open(html_input, encoding="utf8")
     soup = BeautifulSoup(file, 'html.parser')
 
-
-    #dates = soup.findAll('span', {'class' : 'meta'})
-
-
     dates = soup.findAll('div', class_ = '_3-94 _2lem')
-    for date in dates:
-        print(date.get_text())
+    text = soup.findAll('div', class_='_3-96 _2let')
+    author = soup.findAll('div', class_='_3-96 _2pio _2lek _2lel')
 
-    #print(soup.get_text())
+    for k in range(len(text)):
+        print(author[k].get_text())
+        print(dates[k+1].get_text())
+        print(text[k].get_text())
+        print("")
+
+    #reac = soup.findAll('li')
+    # print(reac)
+
     # for date in dates:
     #     data = date.contents[0].split(",", 1)
     #     data = data[1].split("at")
@@ -28,12 +32,12 @@ def write_to_csv(message):
     #     data = " ".join(data)
     #     csvfile.writerow([data])
 
-def count_messages_bydate():
-    df = pd.read_csv('chatdata.csv')
-    df.groupby(df.date).size().to_csv("count.csv", header=True)
-    ab = pd.read_csv('count.csv')
-    ab['date'] = pd.to_datetime(ab.date)
-    ab.sort_values('date').to_csv("sorted.csv", header=True)
+# def count_messages_bydate():
+#     df = pd.read_csv('chatdata.csv')
+#     df.groupby(df.date).size().to_csv("count.csv", header=True)
+#     ab = pd.read_csv('count.csv')
+#     ab['date'] = pd.to_datetime(ab.date)
+#     ab.sort_values('date').to_csv("sorted.csv", header=True)
 
 
 
