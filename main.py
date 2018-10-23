3e452d420359bf1d7d24f7b711c15e1918f4ac0c
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2018-10-23 21:24:17 +0200

    correct syntax to print date of posting

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ from bs4 import BeautifulSoup
 
 
 def write_to_csv(message):
-    csvfile = csv.writer(open("chatdata2.csv", "w"))
-    csvfile.writerow(["date"])
+    #csvfile = csv.writer(open("chatdata.csv", "w"))
+    #csvfile.writerow(["date"])
     file = open(message, encoding="utf8")
-    soup = BeautifulSoup(file, 'lxml')
-    dates = soup.findAll('span', {'class' : 'meta'})
+    soup = BeautifulSoup(file, 'html.parser')
+
+
+    #dates = soup.findAll('span', {'class' : 'meta'})
+
+
+    dates = soup.findAll('div', class_ = '_3-94 _2lem')
     for date in dates:
-        data = date.contents[0].split(",", 1)
-        data = data[1].split("at")
-        data = str(data[0])
-        data = data.split(",")
-        data = " ".join(data)
-        data = data.split(" ")
-        data = " ".join(data)
-        csvfile.writerow([data])
+        print(date.get_text())
+
+    #print(soup.get_text())
+    # for date in dates:
+    #     data = date.contents[0].split(",", 1)
+    #     data = data[1].split("at")
+    #     data = str(data[0])
+    #     data = data.split(",")
+    #     data = " ".join(data)
+    #     data = data.split(" ")
+    #     data = " ".join(data)
+    #     csvfile.writerow([data])
 
 def count_messages_bydate():
     df = pd.read_csv('chatdata.csv')
@@ -28,4 +37,4 @@ def count_messages_bydate():
 
 
 
-write_to_csv("chatdata.csv")
+write_to_csv("messagetest.html")
